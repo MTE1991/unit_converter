3cbd23792e1e6dd2a10038c87f32c5559b17f264
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2021-05-08 13:23:50 +0600

    Add files via upload

---
+++ Converter/MainWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBtnClick(object sender, EventArgs e)
+    {
+        if (this.combobox1.ActiveText == "mile to km")
+        {
+            double val = Convert.ToDouble(this.entry1.Text) * 1.60934; // Input value
+            this.outputlabel.Text = this.entry1.Text + " mile = " + Convert.ToString(val) + " km";
+        }
+        else if (this.combobox1.ActiveText == "km to mile") {
+            double val = Convert.ToDouble(this.entry1.Text) * 0.621371; // Input value
+            this.outputlabel.Text = this.entry1.Text + " km = " + Convert.ToString(val) + " mile";
+        }
+    }
+
+    protected void OnCboxChanged(object sender, EventArgs e)
+    {
+        if (this.combobox1.ActiveText == "km to mile")
+            this.userinputtxt.Text = "km:";
+        else if (this.combobox1.ActiveText == "mile to km")
+            this.userinputtxt.Text = "mile:";
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace Converter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Title = "Unit Converter 1.0";
+            win.Show();
+            Application.Run();
+        }
+    }
+}
